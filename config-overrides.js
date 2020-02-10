f0e650b79437a39c2bf730c9da0f2c6460e323f3
Robson Mathias <robsond.mathias@gmail.com>
2020-02-10 10:26:58 -0300

    Change script

--- config-overrides.js
+++ config-overrides.js
@@ -1,5 +1,12 @@
-module.exports = (config, env) => {
-    config.output.filename = 'static/js/[name].js';
-    config.output.chunkFilename = 'static/js/[name].js';
+module.exports = (config) => {
+    config.output.libraryTarget = 'amd';
+    config.output.filename = 'scripts/[name].js';
+    config.output.chunkFilename = 'scripts/[name].js';
+    config.optimization.splitChunks = {
+        cacheGroups: {
+            default: false
+        }
+    };
+    config.optimization.runtimeChunk = false;
     return config
 };
